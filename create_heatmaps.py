5badc70b2794abac5edf9dad7d8041bf3936a4c3
Marko van Treeck <markovantreeck@gmail.com>
2022-08-15 16:21:33 +0200

    Intermediate

--- create_heatmaps.py
+++ create_heatmaps.py
@@ -19,11 +19,14 @@ import PIL
 from tqdm import tqdm
 import numpy as np
 from fastai.vision.all import load_learner
+import pandas as pd
 
 # use all the threads
 torch.set_num_threads(os.cpu_count())
 torch.set_num_interop_threads(os.cpu_count())
 
+
+slide_dir = Path('/mnt/Sirius_02_empty/CPTAC_IMGS_PATH/CPTAC_BRCA')
 
 def _load_tile(
     slide: openslide.OpenSlide, pos: Tuple[int, int], stride: Tuple[int, int], target_size: Tuple[int, int]
@@ -90,71 +93,76 @@ base_model.fc = nn.Identity()
 base_model.load_state_dict(pretext_model, strict=True)
 base_model.eval()
 
-# transform MIL model into fully convolutional equivalent
-learn = load_learner('/home/mvantreeck/Downloads/export.pkl')
-att = nn.Sequential(
-    linear_to_conv2d(learn.encoder[0]),
-    nn.ReLU(),
-    linear_to_conv2d(learn.attention[0]),
-    nn.Tanh(),
-    linear_to_conv2d(learn.attention[2]),
-)
-score = nn.Sequential(
-    linear_to_conv2d(learn.encoder[0]),
-    nn.ReLU(),
-    linear_to_conv2d(learn.head[3]),
-)
 
+for csv in Path('/home/mvantreeck/Downloads/BRCA').glob('*.csv'):
+    target = csv.name.split('-', 1)[0]
+    df = pd.read_csv(csv).nsmallest(5, columns='loss')
+    for _, (patient_name, preds_path) in df[['patient', 'path']].iterrows():
+        fold = Path(preds_path).parent.name
+        for slide_path in tqdm([slide for slide in slide_dir.glob('*.svs') if patient_name[1:] in slide.name]):
+            print(slide_path)
+            slide = openslide.OpenSlide(str(slide_path))
+            outdir = Path('/mnt/Sirius_02_empty/Oliver_TCGA_runs/BRCA/val_mil_multitarget/')/target/fold/csv.name/slide_path.stem
+            outdir.mkdir(parents=True, exist_ok=True)
+            print(f'saving to {outdir}')
 
-for slide_name in ["LUEDDE_HEART_HTx_IMG_H022557_19"]:
-    slide_path = Path(
-        '/mnt/Jupiter_05_DEEP_LIVER_A/LUEDDE-HEART-HTx/LUEDDE-HEART-HTX-IMGS')/f'{slide_name}.svs'
-    print(slide_path)
-    slide = openslide.OpenSlide(str(slide_path))
-    outdir = Path('heatmaps')/slide_path.stem
-    outdir.mkdir(parents=True, exist_ok=True)
+            # transform MIL model into fully convolutional equivalent
+            learn = load_learner(Path('/mnt/Sirius_02_empty/Oliver_TCGA_runs/BRCA/mil_multitarget')/target/fold/'export.pkl')
+            att = nn.Sequential(
+                nn.AvgPool2d(7, 1),
+                linear_to_conv2d(learn.encoder[0]),
+                nn.ReLU(),
+                linear_to_conv2d(learn.attention[0]),
+                nn.Tanh(),
+                linear_to_conv2d(learn.attention[2]),
+            )
+            score = nn.Sequential(
+                nn.AvgPool2d(7, 1),
+                linear_to_conv2d(learn.encoder[0]),
+                nn.ReLU(),
+                linear_to_conv2d(learn.head[3]),
+            )
 
-    # load WSI as one image
-    slide_t = load_slide(slide)
-    PIL.Image.fromarray(slide_t).save(outdir/f'{slide_path.stem}.jpg')
+            # load WSI as one image
+            slide_t = load_slide(slide)
+            PIL.Image.fromarray(slide_t).save(outdir/f'{slide_path.stem}.jpg')
 
-    # pass the WSI through the fully convolutional network
-    # since our RAM is still too small, we do this in two steps
-    # (if you run out of RAM, try upping the number of slices)
-    no_slices = 2
-    step = slide_t.shape[1]//no_slices
-    slices = []
-    for slice_i in range(no_slices):
-        x = tfms(slide_t[:, slice_i*step:(slice_i+1)*step, :])
-        with torch.inference_mode():
-            slices.append(base_model(x.unsqueeze(0)))
-    feat_t = torch.concat(slices, 3).squeeze()
-    # save the features (large)
-    torch.save(feat_t, outdir/'feats.pkl')
+            # pass the WSI through the fully convolutional network
+            # since our RAM is still too small, we do this in two steps
+            # (if you run out of RAM, try upping the number of slices)
+            no_slices = 2
+            step = slide_t.shape[1]//no_slices
+            slices = []
+            for slice_i in range(no_slices):
+                x = tfms(slide_t[:, slice_i*step:(slice_i+1)*step, :])
+                with torch.inference_mode():
+                    slices.append(base_model(x.unsqueeze(0)))
+            feat_t = torch.concat(slices, 3).squeeze()
+            # save the features (large)
+            torch.save(feat_t, outdir/'feats.pkl')
 
-    # calculate the attentions / scores according to the MIL model
-    with torch.inference_mode():
-        att_map = att(feat_t).squeeze()
-        lower = torch.quantile(att_map, .01)
-        att_map = att_map.where(att_map > lower, lower)
-        att_map -= att_map.min()
-        att_map /= att_map.max()
+            # calculate the attentions / scores according to the MIL model
+            with torch.inference_mode():
+                att_map = att(feat_t).squeeze()
+                lower = torch.quantile(att_map, .01)
+                att_map = att_map.where(att_map > lower, lower)
+                att_map -= att_map.min()
+                att_map /= att_map.max()
 
-        score_map = score(feat_t).squeeze()
-        score_map = torch.softmax(score_map, 0)
+                score_map = score(feat_t).squeeze()
+                score_map = torch.softmax(score_map, 0)
 
-    att_map_im = PIL.Image.fromarray(
-        np.uint8(plt.get_cmap('coolwarm')(att_map)*255.)).convert('RGB')
-    att_map_im.save(outdir/'attention.png')
-    slide_im = PIL.Image.fromarray(slide_t)
-    PIL.Image.blend(slide_im, att_map_im.resize(slide_im.size),
-                    0.75).save(outdir/'attention_overlayed.jpg')
-    im = plt.get_cmap('coolwarm')(score_map[1])
-    im[:, :, 3] = att_map * .8
-    map_im = PIL.Image.fromarray(np.uint8(im*255.))
-    map_im.save(outdir/'map.png')
-    map_im = map_im.resize(slide_im.size, PIL.Image.Resampling.NEAREST)
-    x = slide_im.copy().convert('RGBA')
-    x.paste(map_im, mask=map_im)
-    x.convert('RGB').save(outdir/'map_overlayed.jpg')
-# %%
+            att_map_im = PIL.Image.fromarray(
+                np.uint8(plt.get_cmap('coolwarm')(att_map)*255.)).convert('RGB')
+            att_map_im.save(outdir/'attention.png')
+            slide_im = PIL.Image.fromarray(slide_t)
+            PIL.Image.blend(slide_im, att_map_im.resize(slide_im.size),
+                            0.75).save(outdir/'attention_overlayed.jpg')
+            im = plt.get_cmap('coolwarm')(score_map[1])
+            im[:, :, 3] = att_map * .8
+            map_im = PIL.Image.fromarray(np.uint8(im*255.))
+            map_im.save(outdir/'map.png')
+            map_im = map_im.resize(slide_im.size, PIL.Image.Resampling.NEAREST)
+            x = slide_im.copy().convert('RGBA')
+            x.paste(map_im, mask=map_im)
+            x.convert('RGB').save(outdir/'map_overlayed.jpg')
