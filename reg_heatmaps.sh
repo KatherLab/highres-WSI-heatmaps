c60258b3ddbfbe724e704786c72430552e5e39da
Omar El Nahhas <76590695+Avic3nna@users.noreply.github.com>
2022-11-23 13:14:23 +0100

    deleted test bash script

--- reg_heatmaps.sh
+++
@@ -1,5 +0,0 @@
-python create_heatmaps.py \
-	-m /home/omarelnahhas/omars_hdd/omar/immunoproject_project/top5_regression_binarybalanced/"Leukocyte Fraction"/TCGA-CRC-DX-features/fold-1/*.pkl \
-	-o /home/omarelnahhas/omars_hdd/omar/immunoproject_project/hm_reg \
-	-t "Foo" \
-	B07.9042_IV_O.svs
